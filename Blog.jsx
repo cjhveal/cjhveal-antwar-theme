284b97374fe5985c59b10ae917092d6bbd19adf9
Cody Veal <cveal05@gmail.com>
2015-03-15 12:14:22 -0700

    using minimalist template to speed things up

--- Blog.jsx
+++ Blog.jsx
@@ -1,21 +1,34 @@
 var React = require('react');
 var Router = require('react-router');
-var Link = Router.Link;
-
 var Paths = require('antwar-core/PathsMixin');
+var BlogLink = require('antwar-core/BlogLink');
 var _ = require('lodash');
 
-module.exports = React.createClass({
-  mixins: [
-    Router.State,
-    Paths
-  ],
+var Link = Router.Link;
 
+var Blog = React.createClass({
+
+  mixins: [ Router.State, Paths ],
   render: function() {
+    var posts = _.map(_.sortBy(this.getAllPosts(), 'date').reverse(), function(post, i) {
+      return <li key={'post-' + i}>
+        <h3>
+          <BlogLink post={post}>{post.title}</BlogLink>
+          {post.draft ? <span>Draft</span> : null}
+        </h3>
+
+        <span>{post.date}</span>
+        <p>{post.preview}</p>
+      </li>
+    });
     return (
       <div>
-        <h1> Wow it is a blog! </h1>
+        <h1>Blog Posts</h1>
+        <ul>{posts}</ul>
       </div>
-    )
+    );
   }
+
 });
+
+module.exports = Blog;
